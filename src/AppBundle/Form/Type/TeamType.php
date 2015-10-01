f1d87284c45bc106d98b69d74b8e84289b9a6657
Sameddin Pasizade <sameddin.pasizade@gmail.com>
2015-10-01 13:05:11 +0500

    Adding new forms

---
+++ src/AppBundle/Form/Type/TeamType.php
@@ -0,0 +1,29 @@
+<?php
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class TeamType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('team', 'text', [
+                'attr' => [
+                    'placeholder' => 'Enter your team name'
+                ]
+            ])
+            ->add('country', 'text', [
+                'attr' => [
+                    'placeholder' => 'Enter your country'
+                ]
+            ])
+            ->add('save', 'submit', ['label' => 'Add']);
+    }
+
+    public function getName()
+    {
+        return 'team';
+    }
+}
