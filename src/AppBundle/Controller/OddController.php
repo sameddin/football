e031fc35720a864b027e240c8cd9bac673d3b797
Sameddin Pasizade <sameddin.pasizade@gmail.com>
2015-10-16 03:50:44 +0500

    Add about/contacts/feedback/advertising information in footer

---
+++ src/AppBundle/Controller/OddController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class OddController extends Controller
+{
+    /**
+     * @Route("/about", name="about")
+     */
+    public function aboutAction()
+    {
+        return $this->render('@App/Odd/about.html.twig');
+    }
+
+    /**
+     * @Route("/contacts", name="contacts")
+     */
+    public function contactsAction()
+    {
+        return $this->render('@App/Odd/contacts.html.twig');
+    }
+
+    /**
+     * @Route("/feedback", name="feedback")
+     */
+    public function feedbackAction()
+    {
+        $form = $this->createFormBuilder()
+            ->add('firstName')
+            ->add('lastName')
+            ->add('email')
+            ->add('subject')
+            ->add('message', 'textarea', [
+                'attr' => [
+                    'cols' => 90,
+                    'rows' => 10,
+                ]
+            ])
+            ->add('submit', 'submit', [
+                'label' => 'common.submit',
+                'attr' => [
+                    'class' => 'btn-default',
+                ]
+            ])
+            ->getForm();
+
+        return $this->render('@App/Odd/feedback.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/adv", name="advertising")
+     */
+    public function advAction()
+    {
+        return $this->render('@App/Odd/adv.html.twig');
+    }
+}
