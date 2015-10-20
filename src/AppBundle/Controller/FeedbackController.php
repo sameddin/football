8df9ddc42bd81314250c476e0fcbd36caeff10e4
Sameddin Pasizade <sameddin.pasizade@gmail.com>
2015-10-21 04:06:30 +0500

    Implement emailing feedback messages

--- src/AppBundle/Controller/FeedbackController.php
+++ src/AppBundle/Controller/FeedbackController.php
@@ -5,15 +5,31 @@ namespace AppBundle\Controller;
 use AppBundle\Form\Type\FeedbackType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class FeedbackController extends Controller
 {
     /**
      * @Route("/feedback", name="feedback")
      */
-    public function feedbackAction()
+    public function feedbackAction(Request $request)
     {
         $form = $this->createForm(new FeedbackType());
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $data = $form->getData();
+
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Football App: '.$data['subject'])
+                ->setFrom($data['email'])
+                ->setTo('sameddin.pasizade@gmail.com')
+                ->setBody($data['message']);
+            $this->get('mailer')->send($message);
+
+            return $this->redirectToRoute('feedback');
+        }
 
         return $this->render('@App/Feedback/feedback.html.twig', [
             'form' => $form->createView(),
