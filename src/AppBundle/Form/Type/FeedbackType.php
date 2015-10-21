b03f13002f0d2d644c73a64d0cc85d4b9377765c
Sameddin Pasizade <sameddin.pasizade@gmail.com>
2015-10-21 05:57:53 +0500

    Add validation for feedback

--- src/AppBundle/Form/Type/FeedbackType.php
+++ src/AppBundle/Form/Type/FeedbackType.php
@@ -3,16 +3,36 @@ namespace AppBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class FeedbackType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('email')
-            ->add('subject')
+            ->add('name', 'text', [
+                'constraints' => [
+                    new NotBlank(),
+                ]
+            ])
+            ->add('email', 'email', [
+                'constraints' => [
+                    new NotBlank(),
+                    new Email(),
+                ]
+            ])
+            ->add('subject', 'text', [
+                'constraints' => [
+                    new NotBlank(),
+                ]
+            ])
             ->add('message', 'textarea', [
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 10]),
+                ],
                 'attr' => [
                     'cols' => 90,
                     'rows' => 10,
