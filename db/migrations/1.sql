29c0c7ebc8967956f934446e6e823fa1a0400dd0
Sameddin Pasizade <sameddin.pasizade@gmail.com>
2015-10-31 22:15:58 +0400

    Extract DB constraints into separate rows

--- db/migrations/1.sql
+++ db/migrations/1.sql
@@ -1,46 +1,58 @@
 CREATE TABLE coach
 (
-  id   serial PRIMARY KEY NOT NULL,
-  name varchar            NOT NULL
+    id   serial  NOT NULL,
+    name varchar NOT NULL,
+
+    PRIMARY KEY (id)
 );
 CREATE TABLE country
 (
-  id      serial PRIMARY KEY NOT NULL,
-  country varchar            NOT NULL
+    id      serial  NOT NULL,
+    country varchar NOT NULL,
+
+    PRIMARY KEY (id)
 );
 CREATE TABLE player
 (
-  id         serial PRIMARY KEY NOT NULL,
-  name       varchar            NOT NULL,
-  team_id    bigint,
-  number     bigint,
-  role_id    bigint,
-  nation     varchar            NOT NULL,
-  birth      date               NOT NULL,
-  height     bigint,
-  weight     bigint,
-  match      bigint,
-  goal       bigint,
-  pass       bigint,
-  yellowcard bigint,
-  redcard    bigint,
-  season     bigint
+    id         serial  NOT NULL,
+    name       varchar NOT NULL,
+    team_id    bigint,
+    number     bigint,
+    role_id    bigint,
+    nation     varchar NOT NULL,
+    birth      date    NOT NULL,
+    height     bigint,
+    weight     bigint,
+    match      bigint,
+    goal       bigint,
+    pass       bigint,
+    yellowcard bigint,
+    redcard    bigint,
+    season     bigint,
+
+    PRIMARY KEY (id)
 );
 CREATE TABLE role
 (
-  id   serial PRIMARY KEY NOT NULL,
-  role varchar            NOT NULL
+    id   serial  NOT NULL,
+    role varchar NOT NULL,
+
+    PRIMARY KEY (id)
 );
 CREATE TABLE team
 (
-  id            serial PRIMARY KEY NOT NULL,
-  name          varchar            NOT NULL,
-  tournament_id bigint,
-  coach_id      bigint,
-  country_id    bigint
+    id            serial  NOT NULL,
+    name          varchar NOT NULL,
+    tournament_id bigint,
+    coach_id      bigint,
+    country_id    bigint,
+
+    PRIMARY KEY (id)
 );
 CREATE TABLE tournament
 (
-  id         serial PRIMARY KEY NOT NULL,
-  tournament varchar            NOT NULL
+    id         serial  NOT NULL,
+    tournament varchar NOT NULL,
+
+    PRIMARY KEY (id)
 );
