44485118b9438469c763712640a2e9e9971ac40a
Sameddin Pasizade <sameddin.pasizade@gmail.com>
2015-11-03 04:26:59 +0400

    Add FKs in DB

--- db/migrations/1.sql
+++ db/migrations/1.sql
@@ -1,26 +1,62 @@
 CREATE TABLE coach
 (
-    id   bigserial  NOT NULL,
-    name varchar NOT NULL,
+    id   bigserial NOT NULL,
+    name varchar   NOT NULL,
 
     PRIMARY KEY (id)
 );
 CREATE TABLE country
 (
-    id      bigserial  NOT NULL,
-    country varchar NOT NULL,
+    id      bigserial NOT NULL,
+    country varchar   NOT NULL,
+
+    PRIMARY KEY (id)
+);
+CREATE TABLE tournament
+(
+    id         bigserial NOT NULL,
+    tournament varchar   NOT NULL,
+
+    PRIMARY KEY (id)
+);
+CREATE TABLE match
+(
+    id   bigserial NOT NULL,
+    date timestamp NOT NULL,
+
+    PRIMARY KEY (id)
+);
+CREATE TABLE team
+(
+    id            bigserial NOT NULL,
+    coach_id      bigint,
+    tournament_id bigint,
+    country_id    bigint,
+    date_id       bigint,
+    name          varchar   NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (coach_id) REFERENCES coach,
+    FOREIGN KEY (tournament_id) REFERENCES tournament,
+    FOREIGN KEY (country_id) REFERENCES country,
+    FOREIGN KEY (date_id) REFERENCES match
+);
+CREATE TABLE role
+(
+    id   bigserial NOT NULL,
+    role varchar   NOT NULL,
 
     PRIMARY KEY (id)
 );
 CREATE TABLE player
 (
-    id         bigserial  NOT NULL,
-    name       varchar NOT NULL,
+    id         bigserial NOT NULL,
     team_id    bigint,
-    number     bigint,
     role_id    bigint,
-    nation     varchar NOT NULL,
-    birth      date    NOT NULL,
+    name       varchar   NOT NULL,
+    number     bigint,
+    nation     varchar   NOT NULL,
+    birth      date      NOT NULL,
     height     bigint,
     weight     bigint,
     match      bigint,
@@ -30,37 +66,7 @@ CREATE TABLE player
     redcard    bigint,
     season     bigint,
 
-    PRIMARY KEY (id)
-);
-CREATE TABLE role
-(
-    id   bigserial  NOT NULL,
-    role varchar NOT NULL,
-
-    PRIMARY KEY (id)
-);
-CREATE TABLE team
-(
-    id            bigserial  NOT NULL,
-    name          varchar NOT NULL,
-    tournament_id bigint,
-    coach_id      bigint,
-    country_id    bigint,
-    date_id       bigint,
-
-    PRIMARY KEY (id)
-);
-CREATE TABLE tournament
-(
-    id         bigserial  NOT NULL,
-    tournament varchar NOT NULL,
-
-    PRIMARY KEY (id)
-);
-CREATE TABLE match
-(
-    id   bigserial    NOT NULL,
-    date timestamp NOT NULL,
-
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (team_id) REFERENCES team,
+    FOREIGN KEY (role_id) REFERENCES role
 );
