f568f71c56e2d0a0ba6c591c9ca4cb4d05a55a3c
Sameddin Pasizade <sameddin.pasizade@gmail.com>
2015-12-01 03:18:34 +0400

    Add view of Users

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * @Route("/users")
+ */
+class UserController extends Controller
+{
+    /**
+     * @Route(name="user.list")
+     * @Template
+     */
+    public function listAction()
+    {
+        $users = $this->getDoctrine()
+            ->getRepository('AppBundle:User')
+            ->findAll();
+
+        return [
+            'users' => $users
+        ];
+    }
+}
